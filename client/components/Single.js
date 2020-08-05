bbd73340a99c0f7468a2d6504225c80cf6236781
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-08-05 17:43:53 -0500

    Added comments to be displayes in single photo view and made the form to add new comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -7,10 +7,12 @@ const Single = React.createClass({
         // index of the post
         const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
         const post = this.props.posts[i];
+        const postComments = this.props.comments[this.props.params.postId] || []; 
+
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props} />
-                <Comments></Comments>
+                <Comments postComments={postComments} />
             </div>
         )
     }
