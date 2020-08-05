b17aa34452f89975f3c4b223f77fe5749363490a
Horacio Sanchez <hsanchez@mty.ipointsystems.com>
2020-08-05 16:47:42 -0500

    Video 12. Updated state with reducers. Added onclick function to Like button on photos and made it to increment the number of likes

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -19,7 +19,7 @@ const Photo = React.createClass({
                 <figcaption>
                     <p>{post.caption}</p>
                     <div className="control-buttons">
-                        <button className="likes">&hearts; {post.likes}</button>
+                        <button className="likes" onClick={this.props.increment.bind(null, i)}>&hearts; {post.likes}</button>
                         <Link className="button" to={`/view/${post.code}`}>
                             <span className="comment-count">
                                 <span className="speech-bubble" style={{marginRight:10+'px'}}></span>
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,7 +3,19 @@
 // 2. copy of current state
 
 function posts(state = [], action) {
-    return state;
+    switch(action.type) {
+        case 'INCREMENT_LIKES' :
+            // return the updated state
+            console.log('Inc likes');
+            const i = action.index;
+            return[
+                ...state.slice(0, i),
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1)
+            ];
+        default:
+            return state;
+    }
 }
 
 export default posts;
